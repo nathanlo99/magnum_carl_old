67d517b7b2370ba39dd0abb98fa36994b4438824
Nathan Lo <nathanlo99@hotmail.ca>
2019-08-29 09:10:49 -0400

    Changed ASSERT macros to include WARN

--- src/assert.h
+++ src/assert.h
@@ -13,9 +13,25 @@
   s, __FILE__, __func__, __LINE__)
 
 #if defined(DEBUG)
+  #define WASSERT(expr) \
+    do { \
+      if (!(expr)) { WARN("Test failed: " #expr); exit(1); } \
+    } while (0)
+  #define WASSERT_MSG(expr, ...) \
+    do { \
+      if (!(expr)) { \
+        WARN("Test failed: " #expr); \
+        printf(__VA_ARGS__); \
+        printf("\n"); \
+        exit(1); \
+      } \
+    } while (0)
+  #define WASSERT_IF(cond, expr) WASSERT(!(cond) || (expr));
+  #define WASSERT_IF_MSG(cond, expr, ...) \
+    WASSERT_MSG(!(cond) || (expr), __VA_ARGS__);
   #define ASSERT(expr) \
     do { \
-      if (!(expr)) { ERROR(#expr); exit(1); } \
+      if (!(expr)) { ERROR("Test failed: " #expr); exit(1); } \
     } while (0)
   #define ASSERT_MSG(expr, ...) \
     do { \
@@ -26,25 +42,23 @@
         exit(1); \
       } \
     } while (0)
-  #define ASSERT_IF(cond, expr) \
-    do { \
-      if (cond && !(expr)) { ERROR("Test failed: " #expr); exit(1); } \
-    } while (0)
+  #define ASSERT_IF(cond, expr) ASSERT(!(cond) || (expr));
   #define ASSERT_IF_MSG(cond, expr, ...) \
-    do { \
-      if (cond && !(expr)) { \
-        ERROR("Test failed: " #expr); \
-        printf(__VA_ARGS__); \
-        printf("\n"); \
-        exit(1); \
-      } \
-    } while (0)
+    ASSERT_MSG(!(cond) || (expr), __VA_ARGS__);
 #elif !defined(INVISIBLE_ASSERTS)
+  #define WASSERT(expr) do { if ((!(expr))) __builtin_unreachable(); } while (0)
+  #define WASSERT_MSG(expr, ...) ASSERT(expr)
+  #define WASSERT_IF(cond, expr) WASSERT(!(cond) || (expr));
+  #define WASSERT_IF_MSG(cond, expr, ...) WASSERT(!(cond) || (expr));
   #define ASSERT(expr) do { if ((!(expr))) __builtin_unreachable(); } while (0)
   #define ASSERT_MSG(expr, ...) ASSERT(expr)
   #define ASSERT_IF(cond, expr) ASSERT(!(cond) || (expr));
   #define ASSERT_IF_MSG(cond, expr, ...) ASSERT(!(cond) || (expr));
 #else
+  #define WASSERT(expr) do {} while (0)
+  #define WASSERT_MSG(expr, ...) do {} while (0)
+  #define WASSERT_IF(cond, expr) do {} while (0)
+  #define WASSERT_IF_MSG(cond, expr, ...) do {} while (0)
   #define ASSERT(expr) do {} while (0)
   #define ASSERT_MSG(expr, ...) do {} while (0)
   #define ASSERT_IF(cond, expr) do {} while (0)
